e44692ee8272e77f1ce50f921d51f824a3db52a4
jsalig1 <jeremy.salig@gmail.com>
2016-03-23 16:16:08 -0400

    Add route for new erb
    Add link form index to new, corresponding route and remove leading
    slashes before env variables in routes because they were being
    interpreted as '//' when env was not in use.

--- job_maker.rb
+++ job_maker.rb
@@ -5,7 +5,11 @@ require './lib/job_folder_helper'
 
 Dotenv.load
 
-get "/#{ENV['SUB_DIR']}/" do
+get "#{ENV['SUB_DIR']}/" do
   @job_folders = JobFolderHelper.fetch_job_folders
   erb :index
+end
+
+get "#{ENV['SUB_DIR']}/new" do
+  erb :new
 end
