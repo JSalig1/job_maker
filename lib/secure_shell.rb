c9cc2f5e2851943892e90afd4f5793a575b5fd4d
jsalig1 <jeremy.salig@gmail.com>
2016-04-04 13:44:23 -0400

    Add ssh session commands
    Add ssh session commands so that we now connect via ssh in ernest with
    login credentials set with enviroment variables. Commands generated by
    the app will now execute. With this change in place we can deploy to
    the production server.

--- lib/secure_shell.rb
+++ lib/secure_shell.rb
@@ -1,24 +1,24 @@
 class SecureShell
   def initialize
 
-#    @session = Net::SSH.start(
-#      ENV['SERVER_HOST'],
-#      ENV['SERVER_USER'],
-#      password: ENV['SERVER_PASSWORD']
-#    )
+    @session = Net::SSH.start(
+      ENV['SERVER_HOST'],
+      ENV['SERVER_USER'],
+      password: ENV['SERVER_PASSWORD']
+    )
 
   end
 
   def create_job_folder(folder_name)
     command = generate_copy_to_job_folder + folder_name
     puts command
-    #@session.exec!(command)
-    #@session.close
+    @session.exec!(command)
+    @session.close
   end
 
   private
 
   def generate_copy_to_job_folder
-    "cp -a #{ENV['JOB_TEMPLATE_PATH']} #{ENV['JOBS_FOLDER_PATH']}/"
+    "cp -a #{ENV['JOB_TEMPLATE_PATH']} #{ENV['JOB_TARGET_PATH']}/"
   end
 end
