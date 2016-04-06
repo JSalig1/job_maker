58cf4b76d97d26bc0190ba25a6c4e6105cfcac27
jsalig1 <jeremy.salig@gmail.com>
2016-04-06 15:05:07 -0400

    Sort by downcase
    Previously folder names were not being sorted completely aplhabetically
    because of lowercase and capitalization differences. Now we sort folders
    by their downcased value so that they're alphabetical regardless of
    capitalization.

--- job_maker.rb
+++ job_maker.rb
@@ -13,7 +13,7 @@ enable :sessions
 set :session_secret, ENV['SECRET']
 
 get "#{ENV['SUB_DIR']}/" do
-  @job_folders = JobFolderHelper.fetch_job_folders.sort
+  @job_folders = JobFolderHelper.fetch_job_folders.sort_by(&:downcase)
   erb :index
 end
 
